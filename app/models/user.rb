e4273e5104c2cd5117897c67c8046dd060e69e94
github username <github email address>
2019-03-05 09:16:24 -0500

    poorly written sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_many :user_trails
     has_many :trails, through: :user_trails
     has_many :lists
-    has_many :trails, through: :lists
+    has_many :trail_lists, through: :lists, source: :trail
 end
