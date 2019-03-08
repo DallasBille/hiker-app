aa175164c93833862fb17372f09021d5e1ad2724
github username <github email address>
2019-03-08 12:17:46 -0500

    styled the website app

--- app/models/user_trail.rb
+++ app/models/user_trail.rb
@@ -2,6 +2,6 @@ class UserTrail < ApplicationRecord
     belongs_to :user
     belongs_to :trail
 
-    validates :review, {presence: true}
-    validates :user_id, uniqueness: {scope: :trail_id, message: "You have already reviewed this trail"}
+    validates :review, presence: true
+    validates :user_id, uniqueness: { scope: :trail_id, message: 'You have already reviewed this trail' }
 end
