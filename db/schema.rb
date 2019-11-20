e9cb0a4bd786f1ad1462ccb08a8fd3add7c25770
github username <dallasbille@gmail.com>
2019-11-20 18:38:43 -0500

    small edits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_023737) do
+ActiveRecord::Schema.define(version: 2019_03_05_181501) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "lists", force: :cascade do |t|
     t.integer "trail_id"
